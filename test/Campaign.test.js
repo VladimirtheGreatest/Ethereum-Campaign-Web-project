9f8af31236763072a34ce3ecd1054a23aa38195e
Vladimir <vladkodoktorov@gmail.com>
2021-10-28 14:38:34 +0100

    Add new test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -4,7 +4,7 @@ const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
 const compiledFactory = require("../ethereum/build/CampaignFactory.json");
-const compiledCampaign = require("..ethereum/build/Campaign.json");
+const compiledCampaign = require("../ethereum/build/Campaign.json");
 
 let accounts;
 let factory;
@@ -12,21 +12,27 @@ let campaignAddress;
 let campaign;
 
 beforeEach(async () => {
-  //deploying a new version of a contract
   accounts = await web3.eth.getAccounts();
+
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode })
     .send({ from: accounts[0], gas: "1000000" });
 
-  //creating the actual campaign after creating factory contract
   await factory.methods.createCampaign("100").send({
     from: accounts[0],
     gas: "1000000",
   });
 
   [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
-  campaign = await new web3.eth.Contract( //if we have already deployed a contract we can just pass an interface and an adress
+  campaign = await new web3.eth.Contract(
     JSON.parse(compiledCampaign.interface),
     campaignAddress
   );
+});
+
+describe("Campaigns", () => {
+  it("deploys a factory and a campaign", () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
 });
