a8247f1526f9c970108f1fdb9cd0b170c0fe8ccb
Vladimir <vladkodoktorov@gmail.com>
2021-11-03 13:58:13 +0000

    newtests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -35,4 +35,29 @@ describe("Campaigns", () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+  it("marks the caller as the campaign manager", async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it("allows people to contribute money and mark them as as approvers", async () => {
+    await campaign.methods.contribute().send({
+      value: "200",
+      from: accounts[1],
+    });
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
+
+  it("requires a minimum contribution", async () => {
+    try {
+      campaign.methods.contribute().send({
+        value: "5",
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
 });
